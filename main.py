65b252177f33d4c11b7870f0537ed66801455f5b
Den <denis100703@gmail.com>
2022-01-17 04:26:35 +0300

    теперь фотку можно открыть в стандартном приложении или в проводнике

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow, QFileDialog
 from PyQt5.QtGui import QPixmap
 from datetime import datetime
-import os, sys, glob, math
+import os, sys, glob, math, subprocess
 from PyQt5 import QtCore
 from PIL import Image
 from ui import *
@@ -11,6 +11,10 @@ def convert_size(size_bytes):
    i = int(math.floor(math.log(size_bytes, 1024)))
    s = round(size_bytes / math.pow(1024, i), 2)
    return "%s %s" % (s, ("B", "KB", "MB", "GB")[i])
+
+def getModifyDate(path):
+    ts = os.path.getmtime(path)
+    return "Null" if ts < 0 else datetime.fromtimestamp(ts).strftime('%d.%m.%Y')
 
 class Ui(QMainWindow):
     #TODO: вынести код в удобные красивые функции
@@ -24,6 +28,8 @@ class Ui(QMainWindow):
         self.ui.path_btn.clicked.connect(self.checkPath)
         self.ui.btn_next.clicked.connect(self.nextImage)
         self.ui.btn_prev.clicked.connect(self.prevImage)
+        self.ui.graphicsView.setMouseTracking(True)
+        self.ui.graphicsView.viewport().installEventFilter(self)
 
         # variables
         self.image_id = 0
@@ -37,6 +43,17 @@ class Ui(QMainWindow):
 
     def nextImage(self): self.changeImage(1)
     def prevImage(self): self.changeImage(-1)
+
+    def viewImage(self):
+        if self.image_path != "":
+            os.startfile(self.image_path, 'open')
+
+    def showImageInExplorer(self):
+        if self.image_path != "":
+            subprocess.run([
+                os.path.join(os.getenv('WINDIR'), 'explorer.exe'),
+                '/select,',
+                os.path.normpath(self.image_path)])
 
     def setChildrenFocusPolicy(self, policy):
         def recursiveSetChildFocusPolicy (parentQWidget):
@@ -52,7 +69,7 @@ class Ui(QMainWindow):
         temp = self.pickDirectory()
         if temp != "":
             self.path = temp
-            self.image_list = [item for i in [glob.glob(f"{self.path}\/*{ext}") for ext in ["jpg","jpeg","png"]] for item in i]
+            self.image_list = [item.replace("\\", "/") for i in [glob.glob(f"{self.path}/*{ext}") for ext in ["jpg","jpeg","png"]] for item in i]
             self.im_count = len(self.image_list)
             self.image_id = 0
             self.ui.path_text.setText(f"path: {self.path} ({self.im_count} photo)")
@@ -75,7 +92,19 @@ class Ui(QMainWindow):
                     f"Image {self.image_id + 1} / {self.im_count}\n" +
                     f"Res: {' × '.join([str(i) for i in self.image.size])}\n" +
                     f"Size: {convert_size(os.path.getsize(self.image_path))}\n" +
-                    f"Date: {datetime.fromtimestamp(os.path.getmtime(self.image_path)).strftime('%d.%m.%Y')}") # full: %d.%m.%Y %H:%M:%S
+                    f"Date: {getModifyDate(self.image_path)}") # full: %d.%m.%Y %H:%M:%S
+
+    # def mousePressEvent(self, event):
+    #     if event.button() == QtCore.Qt.LeftButton:
+    #         print("Press!")
+
+    def eventFilter(self, source, event):
+        if event.type() == QtCore.QEvent.MouseButtonPress:
+            if event.button() == QtCore.Qt.LeftButton:
+                self.viewImage()
+            elif event.button() == QtCore.Qt.RightButton:
+                self.showImageInExplorer()
+        return super(Ui, self).eventFilter(source, event)
 
     def changeImage(self, order):
         self.image_id += order
