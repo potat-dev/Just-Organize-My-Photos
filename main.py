3ac0f667c69b9408a98fee8a085f36d6ee00b2ec
Den <denis100703@gmail.com>
2022-01-20 05:02:39 +0300

    пофиксил старые баги, добавил новые

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from datetime import datetime
 import os, sys, glob, math, subprocess
 from PyQt5 import QtCore
 from PIL import Image
+from math import floor, log, pow
 from ui import *
 
 def convert_size(size_bytes):
    if size_bytes == 0: return "0"
-   i = int(math.floor(math.log(size_bytes, 1024)))
-   s = round(size_bytes / math.pow(1024, i), 2)
-   return "%s %s" % (s, ("B", "KB", "MB", "GB")[i])
+   i = int(floor(log(size_bytes, 1024)))
+   s = round(size_bytes / pow(1024, i), 2)
+   return f"{s} {('B','KB','MB','GB')[i]}"
 
 def getModifyDate(path):
     ts = os.path.getmtime(path)
@@ -19,8 +20,17 @@ def getModifyDate(path):
 class Ui(QMainWindow):
     #TODO: вынести код в удобные красивые функции
     #TODO: реализовать алгоритм выбора папки для шортката
+    #TODO: пофиксить баг с невозможностью перемещения в корзину
 
     def __init__(self):
+        # variables
+        self.image_id   = 0
+        self.im_count   = 0
+        self.image_list = []
+        self.path       = ""
+        self.image_path = ""
+        self.image      = None
+        
         super(Ui, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
@@ -28,17 +38,9 @@ class Ui(QMainWindow):
         self.ui.path_btn.clicked.connect(self.checkPath)
         self.ui.btn_next.clicked.connect(self.nextImage)
         self.ui.btn_prev.clicked.connect(self.prevImage)
+        self.ui.btn_del.clicked.connect(self.deleteImage)
         self.ui.graphicsView.setMouseTracking(True)
         self.ui.graphicsView.viewport().installEventFilter(self)
-
-        # variables
-        self.image_id = 0
-        self.im_count = 0
-        self.image_list = []
-        self.path = ""
-        self.image_path = ""
-        self.image = None
-
         self.show()
 
     def nextImage(self): self.changeImage(1)
@@ -75,6 +77,14 @@ class Ui(QMainWindow):
             self.ui.path_text.setText(f"path: {self.path} ({self.im_count} photo)")
             if self.im_count > 0:
                 self.displayImg()
+    
+    def deleteImage(self):
+        del_img = self.image_list.pop(self.image_id)
+        # QtCore.QFile.moveToTrash(del_img)
+        os.remove(del_img) # временное решение
+        self.im_count = len(self.image_list)
+        if self.image_id == self.im_count: self.image_id -= 1
+        self.displayImg()
 
     def displayImg(self):
         if self.im_count > 0:
@@ -86,13 +96,21 @@ class Ui(QMainWindow):
                 item = QtWidgets.QGraphicsPixmapItem(pixmap.scaled(w, h, QtCore.Qt.KeepAspectRatio))
                 scene.addItem(item)
                 self.ui.graphicsView.setScene(scene)
-                self.image = Image.open(self.image_path)
                 self.ui.path_text.setText(f"File: {os.path.basename(self.image_path)}\nPath: {self.path}")
-                self.ui.info_text.setText(
-                    f"Image {self.image_id + 1} / {self.im_count}\n" +
-                    f"Res: {' × '.join([str(i) for i in self.image.size])}\n" +
-                    f"Size: {convert_size(os.path.getsize(self.image_path))}\n" +
-                    f"Date: {getModifyDate(self.image_path)}") # full: %d.%m.%Y %H:%M:%S
+                
+                try:
+                    self.image = Image.open(self.image_path)
+                    self.ui.info_text.setText(
+                        f"Image {self.image_id + 1} / {self.im_count}\n" +
+                        f"Res: {' × '.join([str(i) for i in self.image.size])}\n" +
+                        f"Size: {convert_size(os.path.getsize(self.image_path))}\n" +
+                        f"Date: {getModifyDate(self.image_path)}") # full: %d.%m.%Y %H:%M:%S
+                except OSError:
+                    print("что-то пошло по пизде") # debug
+                    self.ui.info_text.setText(
+                        f"Image {self.image_id + 1} / {self.im_count}\nRes: Null\n" +
+                        f"Size: {convert_size(os.path.getsize(self.image_path))}\nDate: Null\n" +
+                        "[ corrupted image file ]")
 
     # def mousePressEvent(self, event):
     #     if event.button() == QtCore.Qt.LeftButton:
@@ -130,7 +148,30 @@ class Ui(QMainWindow):
         else:
             QMainWindow.keyPressEvent(self, event)
 
-
-app = QApplication([])
-ui = Ui()
+# run app
+app, ui = QApplication([]), Ui()
 sys.exit(app.exec_())
+
+
+## жалкие попытки перемещать файл в корзину ##
+
+# def deleteImage(self):
+#     im_pth = self.image_list.pop(self.image_id)
+#     self.im_count = len(self.image_list)
+#     # print(im_pth)
+#     # print(os.path.realpath(im_pth))
+#     file = QFile(im_pth)
+#     send2trash(str(file.fileName()).replace("/", "\\"))
+#     file.close()
+#     # print(file.moveToTrash())
+#     # print(file.fileName())
+#     # send2trash(os.path.realpath(im_pth))
+#     # QFile.moveToTrash(os.path.realpath(im_pth))
+#     if self.image_id == self.im_count:
+#         self.image_id -= 1
+#     self.displayImg()
+
+# from send2trash import send2trash
+# QtCore.QFile.moveToTrash("D:/Projects/PhotoOrganizer/test_for_del/20210629_193404.jpg")
+# send2trash(os.path.abspath("D:/Projects/PhotoOrganizer/test_for_del/f65240960.jpeg"))
+# def deleteFile(path): QtCore.QFile.moveToTrash(QtCore.QFile(path))
