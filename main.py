d28f8677b3251f78d8afc88b7de0545dda11f7a2
Den <denis100703@gmail.com>
2022-01-27 01:12:30 +0300

    сделал чтобы все было по красоте

--- main.py
+++ main.py
@@ -23,54 +23,38 @@ class Ui(QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()
 
-        self.image_id   = 0
-        self.im_count   = 0
-        self.image_list = []
-        self.path       = ""
-        self.image_path = ""
-        self.image      = None
-        self.scene      = None
-        self.folders    = {}
+        self.image_list, self.folders = [], {}
+        self.image_id, self.img_count = 0, 0
+        self.image, self.scene = None, None
+        self.path, self.image_path = "", ""
 
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.show()
+
         self.setChildrenFocusPolicy(QtCore.Qt.NoFocus)
         self.ui.path_btn.clicked.connect(self.checkPath)
         self.ui.btn_next.clicked.connect(self.nextImage)
         self.ui.btn_prev.clicked.connect(self.prevImage)
         self.ui.btn_del.clicked.connect(self.deleteImage)
-        self.ui.graphicsView.setMouseTracking(True)
-        self.ui.graphicsView.viewport().installEventFilter(self)
+        self.ui.canvas.setMouseTracking(True)
+        self.ui.canvas.viewport().installEventFilter(self)
 
-        self.btn_tags = [str(i) for i in range(1, 10)] + ["0", "Enter", "Space"]
-        self.btn_keys = [Qt.Key_1, Qt.Key_2, Qt.Key_3, Qt.Key_4, Qt.Key_5,
-                         Qt.Key_6, Qt.Key_7, Qt.Key_8, Qt.Key_9, Qt.Key_0,
-                         Qt.Key_Enter, Qt.Key_Space]
-        self.buttons  = [self.ui.bt1, self.ui.bt2, self.ui.bt3, self.ui.bt4,
-                         self.ui.bt5, self.ui.bt6, self.ui.bt7, self.ui.bt8,
-                         self.ui.bt9, self.ui.bt0, self.ui.btE, self.ui.btS]
+        self.tags = [str(i) for i in range(1, 10)] + ["0", "Enter", "Space"]
+        self.keys = [Qt.Key_1, Qt.Key_2, Qt.Key_3, Qt.Key_4, Qt.Key_5,
+                     Qt.Key_6, Qt.Key_7, Qt.Key_8, Qt.Key_9, Qt.Key_0,
+                     Qt.Key_Enter, Qt.Key_Space]
 
-        #! здесь баг: при первом выборе папки, все кнопки
-        #! начинают перемещать фотки именно в эту папку
-        # for btn, tag in zip(self.buttons, self.btn_tags):
-        #     print("обработка кнопки", tag, ":", btn)
-        #     btn.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
-        #     btn.clicked.connect(lambda tag=tag: self.move2folder(folder=tag))
-        #     btn.customContextMenuRequested.connect(lambda checked, tag=tag:
-        #         self.move2folder(folder=tag, change=True))
+        self.buttons = [self.ui.bt1, self.ui.bt2, self.ui.bt3, self.ui.bt4,
+                        self.ui.bt5, self.ui.bt6, self.ui.bt7, self.ui.bt8,
+                        self.ui.bt9, self.ui.bt0, self.ui.btE, self.ui.btS]
 
-        print("настройка обработки нажатий кнопок") #? debug
-        for btn, tag in zip(self.buttons, self.btn_tags):
-            print("обработка кнопки", tag, ":", btn)
+        for btn, tag in zip(self.buttons, self.tags):
             btn.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
             btn.clicked.connect(partial(self.move2folder, folder=tag))
             btn.customContextMenuRequested.connect(partial(self.move2folder, folder=tag, change=True))
 
-        self.ui.path_btn.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
-        self.ui.path_btn.customContextMenuRequested.connect(self.handle_right_click) #? test
-        self.show()
-
-    def handle_right_click(self): print("жопа") #? test
+    def handle_right_click(self): print("test")
     def nextImage(self): self.changeImage(1)
     def prevImage(self): self.changeImage(-1)
 
@@ -95,59 +79,61 @@ class Ui(QMainWindow):
     def pickDirectory(self):
         return str(QFileDialog.getExistingDirectory(self, "Select Directory"))
     
-    def checkPath(self): #! currently not supporting RAW, ARW, ...
-        temp = self.pickDirectory()
-        if temp != "":
-            self.path = temp
-            self.image_list = [item.replace("\\", "/") for i in [glob.glob(f"{self.path}/*{ext}") for ext in ["jpg","jpeg","png"]] for item in i]
-            self.im_count = len(self.image_list)
-            self.image_id = 0
-            self.ui.path_text.setText(f"path: {self.path} ({self.im_count} photo)")
-            if self.im_count > 0:
-                self.displayImg()
+    def checkPath(self):
+        folder = self.pickDirectory()
+        if folder == "": return None
+        self.path = folder
+        self.image_list = [
+            item.replace("\\", "/") for i in [
+                glob.glob(f"{self.path}/*{ext}") for ext in ["jpg","jpeg","png"]
+            ] for item in i
+        ]
+        self.img_count, self.image_id = len(self.image_list), 0
+        self.ui.path_text.setText(f"path: {self.path} ({self.img_count} photo)")
+        if self.img_count > 0:
+            self.displayImg()
     
     def deleteImage(self):
-        if self.im_count > 0:
+        if self.img_count > 0:
             del_img = self.image_list.pop(self.image_id)
             self.image.close()
             QtCore.QFile.moveToTrash(del_img)
-            self.im_count = len(self.image_list)
-            if self.image_id == self.im_count: self.image_id -= 1
-            if self.im_count > 0: self.displayImg()
+            self.img_count = len(self.image_list)
+            if self.image_id == self.img_count: self.image_id -= 1
+            if self.img_count > 0: self.displayImg()
             else: self.clearPreview()
     
     def clearPreview(self):
         self.scene.clear()
-        self.ui.graphicsView.viewport().update()
+        self.ui.canvas.viewport().update()
         self.ui.path_text.setText(f"File: Null\nPath: {self.path}")
         self.ui.info_text.setText(
-            f"Image {self.image_id + 1} / {self.im_count}\n" +
+            f"Image {self.image_id + 1} / {self.img_count}\n" +
             "[ all files sorted ]")
 
     def displayImg(self):
-        if self.im_count > 0:
-            self.image_path = self.image_list[self.image_id]
-            if os.path.isfile(self.image_path):
-                w, h = self.ui.graphicsView.width(), self.ui.graphicsView.height()
-                self.scene = QtWidgets.QGraphicsScene(self)
-                pixmap = QPixmap(self.image_path)
-                item = QtWidgets.QGraphicsPixmapItem(pixmap.scaled(w, h, QtCore.Qt.KeepAspectRatio))
-                self.scene.addItem(item)
-                self.ui.graphicsView.setScene(self.scene)
-                self.ui.path_text.setText(f"File: {os.path.basename(self.image_path)}\nPath: {self.path}")
-                
-                try:
-                    self.image = Image.open(self.image_path)
-                    self.ui.info_text.setText(
-                        f"Image {self.image_id + 1} / {self.im_count}\n" +
-                        f"Res: {' × '.join([str(i) for i in self.image.size])}\n" +
-                        f"Size: {convert_size(os.path.getsize(self.image_path))}\n" +
-                        f"Date: {getModifyDate(self.image_path)}") # full: %d.%m.%Y %H:%M:%S
-                except OSError:
-                    self.ui.info_text.setText(
-                        f"Image {self.image_id + 1} / {self.im_count}\nRes: Null\n" +
-                        f"Size: {convert_size(os.path.getsize(self.image_path))}\nDate: Null\n" +
-                        "[ corrupted image file ]")
+        if self.img_count <= 0: return None
+        self.image_path = self.image_list[self.image_id]
+        if os.path.isfile(self.image_path):
+            w, h = self.ui.canvas.width(), self.ui.canvas.height()
+            self.scene = QtWidgets.QGraphicsScene(self)
+            pixmap = QPixmap(self.image_path)
+            item = QtWidgets.QGraphicsPixmapItem(pixmap.scaled(w, h, QtCore.Qt.KeepAspectRatio))
+            self.scene.addItem(item)
+            self.ui.canvas.setScene(self.scene)
+            self.ui.path_text.setText(f"File: {os.path.basename(self.image_path)}\nPath: {self.path}")
+            try:
+                self.image = Image.open(self.image_path)
+                self.ui.info_text.setText(
+                    f"Image {self.image_id + 1} / {self.img_count}\n" +
+                    f"Res: {' × '.join([str(i) for i in self.image.size])}\n" +
+                    f"Size: {convert_size(os.path.getsize(self.image_path))}\n" +
+                    f"Date: {getModifyDate(self.image_path)}") # full: %d.%m.%Y %H:%M:%S
+            except OSError:
+                self.ui.info_text.setText(
+                    f"Image {self.image_id + 1} / {self.img_count}\nRes: Null\n" +
+                    f"Size: {convert_size(os.path.getsize(self.image_path))}\nDate: Null\n" +
+                    "[ corrupted image file ]")
 
     def eventFilter(self, source, event):
         if event.type() == QtCore.QEvent.MouseButtonPress:
@@ -159,37 +145,29 @@ class Ui(QMainWindow):
 
     def changeImage(self, order):
         self.image_id += order
-        if self.image_id == self.im_count:
+        if self.image_id == self.img_count:
             self.image_id = 0
         elif self.image_id < 0:
-            self.image_id = self.im_count-1
+            self.image_id = self.img_count-1
         self.displayImg()
 
     def move2folder(self, folder, change=False):
-        #TODO: сделать побольше принтов для отладки
-        #TODO: выловить баг с неправильной обработкой нажатий кнопок интерфейса
-        if change: print("баабьмбьламабьабмабьбабаьмбаьбмббамбба")
-
-        if self.im_count <= 0: return None
+        if self.img_count <= 0: return None
         modifier = QtWidgets.QApplication.keyboardModifiers()
-        # print(folder not in self.folders.keys(), "\n", modifier) 
-        if change \
-        or folder not in self.folders.keys() \
-        or modifier == QtCore.Qt.ShiftModifier \
-        or modifier == QtCore.Qt.ControlModifier:
+        if change or folder not in self.folders.keys() \
+        or modifier in (Qt.ControlModifier, Qt.ShiftModifier):
             path = self.pickDirectory()
-            if path == "" : return None # проверка, выбрал ли пользователь папку
+            if path == "" : return None
             self.folders |= {folder: path}
-            print(folder) #? test
-            self.buttons[self.btn_tags.index(folder)].setText(folder + "\n" + os.path.basename(path))
+            self.buttons[self.tags.index(folder)].setText(folder + "\n" + os.path.basename(path))
 
         if folder in self.folders.keys():
             self.image.close() # нужно закрывать файл перед перемещением
             img_pth = self.image_list.pop(self.image_id)
             os.replace(img_pth, os.path.join(self.folders[folder], os.path.basename(img_pth)))
-            self.im_count = len(self.image_list)
-            if self.image_id == self.im_count: self.image_id -= 1
-            if self.im_count > 0: self.displayImg()
+            self.img_count = len(self.image_list)
+            if self.image_id == self.img_count: self.image_id -= 1
+            if self.img_count > 0: self.displayImg()
             else: self.clearPreview()
 
     def keyPressEvent(self, event):
@@ -197,16 +175,9 @@ class Ui(QMainWindow):
         if k == QtCore.Qt.Key_Delete:  self.deleteImage()
         elif k == QtCore.Qt.Key_Right: self.changeImage(1)
         elif k == QtCore.Qt.Key_Left:  self.changeImage(-1)
-        
-        elif k in self.btn_keys:
-            self.move2folder(self.btn_tags[self.btn_keys.index(k)])
-
+        elif k in self.keys: self.move2folder(self.tags[self.keys.index(k)])
         else: QMainWindow.keyPressEvent(self, event)
 
 
 app, ui = QApplication([]), Ui()
 sys.exit(app.exec_())
-
-# def mousePressEvent(self, event): # test
-#     if event.button() == QtCore.Qt.LeftButton:
-#         print("Press!")
